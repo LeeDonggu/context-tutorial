c779cd6fe0b61fe82f42029a04f494ca05991a42
ldg <7sus7@naver.com>
2020-08-17 15:11:57 +0900

    useContext Apply Success

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,29 +1,48 @@
-import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import React, { useContext } from 'react';
+import ColorContext from '../contexts/color';
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {/* value.state 구조분해 할당 사용 */}
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: '64px',
-              height: '64px',
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: '32px',
-              height: '32px',
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    // <ColorConsumer>
+    //   {/* value.state 구조분해 할당 사용 */}
+    //   {({ state }) => (
+    //     <>
+    //       <div
+    //         style={{
+    //           width: '64px',
+    //           height: '64px',
+    //           background: state.color,
+    //         }}
+    //       />
+    //       <div
+    //         style={{
+    //           width: '32px',
+    //           height: '32px',
+    //           background: state.subcolor,
+    //         }}
+    //       />
+    //     </>
+    //   )}
+    // </ColorConsumer>
+
+    // useContext 적용
+    <>
+      <div
+        style={{
+          width: '64px',
+          height: '64px',
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
